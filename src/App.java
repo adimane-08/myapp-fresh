4ff422239e0e514cbc7e83a049e964313ac504e6
Aditya <adityamane40@yahoo.in>
2025-09-26 11:25:24 +0530

    testing java file

--- src/App.java
+++ src/App.java
@@ -13,7 +13,7 @@ public class App {
         server.createContext("/", new HttpHandler() {
             @Override
             public void handle(HttpExchange exchange) throws IOException {
-                String response = "Hello, Guys!";
+                String response = "Hello, World!";
                 exchange.sendResponseHeaders(200, response.getBytes().length);
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
