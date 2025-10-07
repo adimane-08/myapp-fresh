caaae41dbfa155dab638f7af0fb131c6de23ede4
Aditya <adityamane40@yahoo.in>
2025-10-07 16:30:42 +0530

    added message

--- src/App.java
+++ src/App.java
@@ -13,7 +13,7 @@ public class App {
         server.createContext("/", new HttpHandler() {
             @Override
             public void handle(HttpExchange exchange) throws IOException {
-                String response = "Hello, World!";
+                String response = "Hello, testing!";
                 exchange.sendResponseHeaders(200, response.getBytes().length);
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
